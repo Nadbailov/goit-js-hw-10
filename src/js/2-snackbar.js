f8ea7240414cd9c0e32a218a12fe3bc84d7517f5
Nadbailov <nadbailov@gmail.com>
2024-07-19 22:39:25 +0300

    Ваше повідомлення про комітcommit

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,39 @@
-console.log("snacbar");
+// console.log("snacbar");
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const delay = parseInt(event.target.delay.value);
+    const state = event.target.state.value;
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+
+    promise
+        .then((delay) => {
+            console.log(`✅ Fulfilled promise in ${delay}ms`);
+            iziToast.success({
+                title: 'Success',
+                message: `✅ Fulfilled promise in ${delay}ms`,
+                position: 'topRight',
+            });
+        })
+        .catch((delay) => {
+            console.log(`❌ Rejected promise in ${delay}ms`);
+            iziToast.error({
+                title: 'Error',
+                message: `❌ Rejected promise in ${delay}ms`,
+                position: 'topRight',
+            });
+        });
+    event.target.reset();
+});
